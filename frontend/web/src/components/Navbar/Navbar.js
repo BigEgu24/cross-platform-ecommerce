eb5987b46e0dbd570a2a61b7cf8509258d38e250
Eddie Gee <bigegu24@Eddies-MacBook-Air.local>
2021-04-12 04:39:33 -0700

    Add Shared Package

---
+++ frontend/web/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Navbar.scss";
+
+export default function Navbar() {
+    return (
+        <>
+            <header>
+                Navbar!
+            </header>
+        </>
+    )
+}
