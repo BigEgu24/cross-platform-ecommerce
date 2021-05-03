9b8cae5e4c1344e23827736ba97bb8324139226c
Eduardo Gutierrez <bigegu24@gmail.com>
2021-05-02 19:34:27 -0700

    Utils Reducer Created!

---
+++ frontend/shared/utils/Reducers/UtilsReducer.js
@@ -0,0 +1,24 @@
+export const UTILS_ACTIONS = {
+  SET_SPLASH: 'SET_SPLASH',
+  SET_LANG: 'SET_LANG'
+};
+export const utilsState = {
+  splash: true,
+  language: 'en'
+};
+export const utilsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_SPLASH':
+      return {
+        splash: action.payload
+      };
+
+    case 'SET_LANG':
+      return {
+        language: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
