dc3eabcb95a115b08476f0e3060ec4de0b0dbaaf
Eduardo Gutierrez <bigegu24@gmail.com>
2021-05-11 20:40:21 -0700

    update: Product component updated

--- frontend/shared/Product/Product.js
+++ frontend/shared/Product/Product.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from "react-native";
-export default function Product(props) {
-  return /*#__PURE__*/React.createElement(View, {
-    style: styles.container
-  }, /*#__PURE__*/React.createElement(Text, null, "Product"));
-}
+import StarRating from 'react-native-star-rating';
+export const Department = props => {
+  // Pass the param props to the product
+  console.log(props);
+  return /*#__PURE__*/React.createElement(View, null, /*#__PURE__*/React.createElement(Text, null, props.department));
+};
+export const Title = props => {
+  return /*#__PURE__*/React.createElement(View, null, /*#__PURE__*/React.createElement(Text, null, props.title));
+};
+export const Price = props => {
+  return /*#__PURE__*/React.createElement(View, null, /*#__PURE__*/React.createElement(Text, null, props.price.toFixed(2)));
+};
+export const Ratings = props => {
+  return /*#__PURE__*/React.createElement(View, null);
+};
 const styles = StyleSheet.create({
   container: {
     width: 230,
--- frontend/shared/src/Product/Product.js
+++ frontend/shared/src/Product/Product.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import {View, Text, StyleSheet} from "react-native";
+import StarRating from 'react-native-star-rating';
 
-export default function Product(props) {
+export const Department = (props) => {
+    // Pass the param props to the product
+    console.log(props)
     return (
-        <View style={styles.container}>
-            <Text>Product</Text>
+        <View>
+            <Text>{props.department}</Text>
         </View>
     )
 }
+export const Title = (props) => {
+    return (
+        <View>
+            <Text>{props.title}</Text>
+        </View>
+    )
+}
+
+export const Price = (props) => {
+    return (
+        <View>
+            <Text>{props.price.toFixed(2)}</Text>
+        </View>
+    )
+}
+
+export const Ratings = (props) => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         width: 230,
