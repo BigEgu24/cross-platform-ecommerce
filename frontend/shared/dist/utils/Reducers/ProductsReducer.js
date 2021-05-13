082ec6a16294f76b3fe56952f252bba0128f37a1
Eduardo Gutierrez <bigegu24@gmail.com>
2021-05-12 23:48:04 -0700

    Product Reducer created

--- frontend/shared/dist/utils/Reducers/ProductsReducer.js
+++ frontend/shared/dist/utils/Reducers/ProductsReducer.js
@@ -1,16 +1,28 @@
 export const PRODUCTS_ACTIONS = {
   SET_PRODUCTS: 'SET_PRODUCTS',
+  SELECTED_PRODUCT: 'SELECTED_PRODUCT',
   SEARCH_PROD: 'SEARCH_PROD'
 };
 export const productsState = {
   products: [],
-  search: []
+  search: [],
+  product: {
+    department: '',
+    title: '',
+    price: 0.00,
+    rating: 0
+  }
 };
 export const productsReducer = (state, action) => {
   switch (action.type) {
     case 'SET_PRODUCTS':
       return {
         products: action.payload
+      };
+
+    case 'SELECTED_PRODUCT':
+      return {
+        product: action.payload
       };
 
     case 'SEARCH_PROD':
