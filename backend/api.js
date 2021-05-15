25461dbd488289bbff83f9609e3681ecf47ee945
Eduardo Gutierrez <bigegu24@gmail.com>
2021-05-14 19:37:34 -0700

    make it better1

--- backend/api.js
+++ backend/api.js
@@ -7,14 +7,14 @@ const app = express();
 require('dotenv').config({ path: path.resolve(__dirname + '/../.env') })
 const PORT = 3000;
 
-// const db = mysql.createConnection({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME
-// })
-// db.connect();
-console.log(`Hello: ${process.env.DB_USER}`)
+const db = mysql.createConnection({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+db.connect();
+
 // Allow Access to API b64f5b78fe25ad@us-cdbr-east-03.cleardb.com:3306 .
 app.use(cors())
 // Allow API to use JSON
@@ -26,38 +26,38 @@ app.use(express.json())
 
 // Routes
 // Get All The Products
-// app.get("/api/products", (req, res) => {
-//     db.query("SELECT * FROM products", (err, result) => {
-//         if(err){
-//             console.log(err)
-//         }else{
-//             res.json(result)
-//         }
-//     })
-// })
+app.get("/api/products", (req, res) => {
+    db.query("SELECT * FROM products", (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
 
-// // Insert Product
-// app.get("/api/products/insert", (req, res) => {
-//     db.query("INSERT INTO products (id, title) VALUES ('5', 'hello')", (err, result) => {
-//         if(err){
-//             console.log(err)
-//         }else{
-//             res.send("Product Saved!")
-//         }
-//     })
-// })
+// Insert Product
+app.get("/api/products/insert", (req, res) => {
+    db.query("INSERT INTO products (id, title) VALUES ('5', 'hello')", (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send("Product Saved!")
+        }
+    })
+})
 
-// // Get Product Department
-// app.post("/api/product/get", (req, res) => {
+// Get Product Department
+app.post("/api/product/get", (req, res) => {
     
-//     db.query(`SELECT * FROM products WHERE id='${req.body.id}'`, (err, result) => {
-//         if(err){
-//             console.log(err)
-//         }else{
-//             res.json(result)
-//         }
-//     })
-// })
+    db.query(`SELECT * FROM products WHERE id='${req.body.id}'`, (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
 
 // Hashing
 // const secret = '123456';
